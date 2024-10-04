7da5418c091c0daaf102ec4efb8563b5a3eecfbf
Rana Badran <ranaz@Ranas-MacBook-Pro.local>
2024-10-04 17:00:19 -0400

    create searchbar component and add packages

--- src/pages/ResourcePage/ResourcePage.jsx
+++ src/pages/ResourcePage/ResourcePage.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import React, { useState, useEffect, useCallback, useMemo } from "react";
 import NavBar from "../../components/NavBar/NavBar";
+import SearchBar from "../../components/SearchBar/SearchBar";
 import ResourceDetailCard from "../../components/ResourceDetailCard/ResourceDetailCard";
 import ResourceList from "../../components/ResourceList/ResourceList";
 import "./ResourcePage.scss";
@@ -8,47 +9,49 @@ import { collection, getDocs } from "firebase/firestore";
 import { database } from "../../config/firebase";
 
 export default function ResourcePage({ currentUser, onBookmarkUpdate }) {
-  const [resources, setResources] = useState([]);
-  const [selectedResource, setSelectedResource] = useState(null);
-  const [bookmarkedResources, setBookmarkedResources] = useState({});
-  const [category, setCategory] = useState("All");
-  const [type, setType] = useState("");
-  const [level, setLevel] = useState("");
-  const [estDuration, setEstDuration] = useState("");
-  const [commentCounts, setCommentCounts] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
+  
+	const [resources, setResources] = useState([]);
+	const [selectedResource, setSelectedResource] = useState(null);
+	const [bookmarkedResources, setBookmarkedResources] = useState({});
+	const [category, setCategory] = useState("All");
+	const [type, setType] = useState("");
+	const [level, setLevel] = useState("");
+	const [estDuration, setEstDuration] = useState("");
+  const [search, setSearch] = useState("")
+	const [commentCounts, setCommentCounts] = useState({});
+	const [isLoading, setIsLoading] = useState(true);
 
-  // console.log("RESOURCES: ", resources);
-  // Fetching all resources and comments only once
-  useEffect(() => {
-    const getAllResourcesAndComments = async () => {
-      setIsLoading(true);
-      try {
-        const resourcesSnapshot = await getDocs(
-          collection(database, "Resources")
-        );
-        const commentsSnapshot = await getDocs(
-          collection(database, "Comments")
-        );
+    
+	// Fetching all resources and comments only once
+	useEffect(() => {
+		const getAllResourcesAndComments = async () => {
+			setIsLoading(true);
+			try {
+				const resourcesSnapshot = await getDocs(
+					collection(database, "Resources")
+				);
+				const commentsSnapshot = await getDocs(
+					collection(database, "Comments")
+				);
 
-        const resourcesCollection = resourcesSnapshot.docs.map((doc) => {
-          const resourceData = { id: doc.id, ...doc.data() };
-          const resourceComments = commentsSnapshot.docs
-            .filter((commentDoc) => commentDoc.data().resourceId === doc.id)
-            .map((commentDoc) => ({ id: commentDoc.id, ...commentDoc.data() }));
-          return {
-            ...resourceData,
-            comments: resourceComments,
-            commentsCount: resourceComments.length,
-          };
-        });
+				const resourcesCollection = resourcesSnapshot.docs.map((doc) => {
+					const resourceData = { id: doc.id, ...doc.data() };
+					const resourceComments = commentsSnapshot.docs
+						.filter((commentDoc) => commentDoc.data().resourceId === doc.id)
+						.map((commentDoc) => ({ id: commentDoc.id, ...commentDoc.data() }));
+					return {
+						...resourceData,
+						comments: resourceComments,
+						commentsCount: resourceComments.length,
+					};
+				});
 
-        setResources(resourcesCollection);
+				setResources(resourcesCollection);
 
-        // Automatically selects the first resource
-        if (resourcesCollection.length > 0) {
-          setSelectedResource(resourcesCollection[0]);
-        }
+				// Automatically selects the first resource
+				if (resourcesCollection.length > 0) {
+					setSelectedResource(resourcesCollection[0]);
+				}
 
         // Initialize bookmarked resources from local storage
         const savedBookmarks =
@@ -59,15 +62,15 @@ export default function ResourcePage({ currentUser, onBookmarkUpdate }) {
         }, {});
         setBookmarkedResources(bookmarkedState);
 
-        setIsLoading(false);
-      } catch (err) {
-        console.error("Error fetching resources and comments: ", err);
-        setIsLoading(false);
-      }
-    };
+				setIsLoading(false);
+			} catch (err) {
+				console.error("Error fetching resources and comments: ", err);
+				setIsLoading(false);
+			}
+		};
 
-    getAllResourcesAndComments();
-  }, []);
+		getAllResourcesAndComments();
+	}, []);
 
   const handleSelectResource = useCallback(
     (clickedId) => {
@@ -83,8 +86,8 @@ export default function ResourcePage({ currentUser, onBookmarkUpdate }) {
     [resources]
   );
 
-  const handleToggleBookmarked = () => {
-    if (!selectedResource) return;
+	const handleToggleBookmarked = () => {
+		if (!selectedResource) return;
 
     const newBookmarkedState = !bookmarkedResources[selectedResource.id];
     setBookmarkedResources((prev) => ({
@@ -92,7 +95,7 @@ export default function ResourcePage({ currentUser, onBookmarkUpdate }) {
       [selectedResource.id]: newBookmarkedState,
     }));
 
-    let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+		let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
 
     if (newBookmarkedState) {
       bookmarks.push(selectedResource);
@@ -102,9 +105,9 @@ export default function ResourcePage({ currentUser, onBookmarkUpdate }) {
       );
     }
 
-    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
-    onBookmarkUpdate(bookmarks);
-  };
+		localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+		onBookmarkUpdate(bookmarks);
+	};
 
   const handleFilterChange = ({ type, level, estDuration }) => {
     setType(type === "All" || type === "" ? [] : [type]);
@@ -114,16 +117,16 @@ export default function ResourcePage({ currentUser, onBookmarkUpdate }) {
     );
   };
 
-  const handleResourceUpdate = useCallback((updatedResource) => {
-    setResources((prevResources) =>
-      prevResources.map((resource) =>
-        resource.id === updatedResource.id
-          ? { ...resource, ...updatedResource }
-          : resource
-      )
-    );
-    setSelectedResource((prev) => ({ ...prev, ...updatedResource }));
-  }, []);
+	const handleResourceUpdate = useCallback((updatedResource) => {
+		setResources((prevResources) =>
+			prevResources.map((resource) =>
+				resource.id === updatedResource.id
+					? { ...resource, ...updatedResource }
+					: resource
+			)
+		);
+		setSelectedResource((prev) => ({ ...prev, ...updatedResource }));
+	}, []);
 
   const handleCommentAdded = useCallback((resourceId, newComment) => {
     setResources((prevResources) =>
@@ -138,67 +141,73 @@ export default function ResourcePage({ currentUser, onBookmarkUpdate }) {
       )
     );
 
-    if (selectedResource && selectedResource.id === resourceId) {
-      setSelectedResource((prevSelected) => ({
-        ...prevSelected,
-        comments: [...(prevSelected.comments || []), newComment],
-        commentsCount: (prevSelected.commentsCount || 0) + 1,
-      }));
-    }
-  }, []);
-  // Filter resources based on the category, type, skill, and duration
-  const filteredResources = useMemo(() => {
-    return resources.filter((resource) => {
-      const currentCategory =
-        category === "All" || resource.discipline === category;
-      const matchesType = type.length === 0 || type.includes(resource.type);
+		if (selectedResource && selectedResource.id === resourceId) {
+			setSelectedResource((prevSelected) => ({
+				...prevSelected,
+				comments: [...(prevSelected.comments || []), newComment],
+				commentsCount: (prevSelected.commentsCount || 0) + 1,
+			}));
+		}
+	}, []);
+	// Filter resources based on the category, type, skill, and duration
+	const filteredResources = useMemo(() => {
+		return resources.filter((resource) => {
+			const currentCategory =
+				category === "All" || resource.discipline === category;
+			const matchesType = type.length === 0 || type.includes(resource.type);
 
       const matchesLevel = level.length === 0 || level.includes(resource.level);
       const matchesEstDuration =
         estDuration.length === 0 || estDuration.includes(resource.estDuration);
+		const includesSearchTerm = resource.title.toLowerCase().includes(search.toLocaleLowerCase())
 
       return (
-        currentCategory && matchesType && matchesLevel && matchesEstDuration
+        currentCategory && matchesType && matchesLevel && matchesEstDuration && includesSearchTerm
       );
     });
-  }, [resources, category, type, level, estDuration]);
+  }, [resources, category, type, level, estDuration, search]);
 
-  return (
-    <div className="resource__container">
-      <div className="resource__navbar-container">
-        <NavBar
-          onCategoryChange={setCategory}
-          onFormSubmit={(newResource) =>
-            setResources([...resources, newResource])
-          }
-          onFilterChange={handleFilterChange}
-          currentUser={currentUser}
-        />
-      </div>
+	return (
+		<div className="resource__container">
+			<div className="resource__navbar-container">
+				<NavBar
+					onCategoryChange={setCategory}
+					onFormSubmit={(newResource) =>
+						setResources([...resources, newResource])
+					}
+					onFilterChange={handleFilterChange}
+					currentUser={currentUser}
+				/>
+			</div>
+			<div className="search__bar">
+				<SearchBar searchTerm={search} onSearch={setSearch} />
+			<div className="resource__container">
       <div className="resource__cards">
-        <ResourceList
-          resources={filteredResources}
-          selectResource={handleSelectResource}
-          activeResourceId={selectedResource?.id}
-          commentCounts={commentCounts}
-        />
+				<ResourceList
+					resources={filteredResources}
+					selectResource={handleSelectResource}
+					activeResourceId={selectedResource?.id}
+					commentCounts={commentCounts}
+				/>
+			</div>
+			<div className="resource-details__container">
+				{!isLoading && selectedResource && (
+					<ResourceDetailCard
+						selectedResource={selectedResource}
+						handleToggleBookmarked={handleToggleBookmarked}
+						savedBookmarks={Object.values(bookmarkedResources).some(Boolean)}
+						isBookmarked={bookmarkedResources[selectedResource.id] || false}
+						comments={selectedResource.comments}
+						currentUser={currentUser}
+						onResourceUpdate={handleResourceUpdate}
+						onCommentAdded={handleCommentAdded}
+					/>
+				)}
+			</div>
       </div>
-      <div className="resource-details__container">
-        {!isLoading && selectedResource && (
-          <ResourceDetailCard
-            selectedResource={selectedResource}
-            handleToggleBookmarked={handleToggleBookmarked}
-            savedBookmarks={Object.values(bookmarkedResources).some(Boolean)}
-            isBookmarked={bookmarkedResources[selectedResource.id] || false}
-            comments={selectedResource.comments}
-            currentUser={currentUser}
-            onResourceUpdate={handleResourceUpdate}
-            onCommentAdded={handleCommentAdded}
-          />
-        )}
       </div>
-    </div>
-  );
+		</div>
+	);
 }
 
 ResourcePage.propTypes = {
